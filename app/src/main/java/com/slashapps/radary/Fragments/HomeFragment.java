3d5469b692ffa843067669e372dfdeb66065694c
ASMAAmo <asmaa_mostafa24@yahoo.com>
2018-12-19 22:50:27 +0200

    18  Dec 2018 Login screen

--- app/src/main/java/com/slashapps/radary/Fragments/HomeFragment.java
+++ app/src/main/java/com/slashapps/radary/Fragments/HomeFragment.java
@@ -1,26 +1,117 @@
 package com.slashapps.radary.Fragments;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.slashapps.radary.R;
 
 
-public class HomeFragment extends Fragment {
-
-
-
+public class HomeFragment extends Fragment  implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+    View v;
+    private GoogleMap mMap;
+    GoogleApiClient mpiclients;
+    Location mlastLocation;
+    LocationRequest mLocationrequest;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        v= inflater.inflate(R.layout.fragment_home, container,
+                false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(HomeFragment.this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationrequest = new LocationRequest();
+        mLocationrequest.setInterval(1000);
+        mLocationrequest.setFastestInterval(1000);
+        mLocationrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+       // LocationServices.FusedLocationApi.requestLocationUpdates(mpiclients, mLocationrequest, getActivity());
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mlastLocation= location;
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+
+       /* if (mMap!=null){
+            Marker hamburg = mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title("Hamburg"));
+            Marker kiel = mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title("Kiel")
+                    .snippet("Kiel is cool")
+                    .icon(BitmapDescriptorFactory
+                            .fromResource(R.drawable.common_full_open_on_phone)));
+        }*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+// Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        buildGoogleapiClient();
+        mMap.setMyLocationEnabled(true);
+
+    }
+    protected synchronized void buildGoogleapiClient() {
+        mpiclients = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mpiclients.connect();
+
+
+    }
 }
