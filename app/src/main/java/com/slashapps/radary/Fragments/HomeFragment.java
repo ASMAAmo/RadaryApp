8d97e6d1beccc333774b6be7336d99e405a06eb5
ASMAAmo <asmaa_mostafa24@yahoo.com>
2018-12-20 03:41:29 +0200

    Fix Map issue

--- app/src/main/java/com/slashapps/radary/Fragments/HomeFragment.java
+++ app/src/main/java/com/slashapps/radary/Fragments/HomeFragment.java
@@ -1,46 +1,65 @@
 package com.slashapps.radary.Fragments;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.slashapps.radary.R;
 
 
-public class HomeFragment extends Fragment  implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
     View v;
-    private GoogleMap mMap;
+    public static GoogleMap map;
     GoogleApiClient mpiclients;
     Location mlastLocation;
     LocationRequest mLocationrequest;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(getActivity());
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        v= inflater.inflate(R.layout.fragment_home, container,
-                false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(HomeFragment.this);
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
         // Inflate the layout for this fragment
         return v;
+
+
     }
 
 
@@ -50,19 +69,6 @@ public class HomeFragment extends Fragment  implements OnMapReadyCallback, Googl
         mLocationrequest.setInterval(1000);
         mLocationrequest.setFastestInterval(1000);
         mLocationrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-       // LocationServices.FusedLocationApi.requestLocationUpdates(mpiclients, mLocationrequest, getActivity());
-
     }
 
     @Override
@@ -79,39 +85,51 @@ public class HomeFragment extends Fragment  implements OnMapReadyCallback, Googl
     public void onLocationChanged(Location location) {
         mlastLocation= location;
         LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-
-       /* if (mMap!=null){
-            Marker hamburg = mMap.addMarker(new MarkerOptions()
-                    .position(latLng)
-                    .title("Hamburg"));
-            Marker kiel = mMap.addMarker(new MarkerOptions()
-                    .position(latLng)
-                    .title("Kiel")
-                    .snippet("Kiel is cool")
-                    .icon(BitmapDescriptorFactory
-                            .fromResource(R.drawable.common_full_open_on_phone)));
-        }*/
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-
-// Zoom in, animating the camera.
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        // Zoom in the Google Map
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        map.setMyLocationEnabled(true);
         buildGoogleapiClient();
-        mMap.setMyLocationEnabled(true);
-
+        getDeviceLocation();
     }
+
     protected synchronized void buildGoogleapiClient() {
         mpiclients = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
         mpiclients.connect();
 
 
+    }
+
+    private void getDeviceLocation() {
+        try {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            Location location = task.getResult();
+                            LatLng currentLatLng = new LatLng(location.getLatitude(),
+                                    location.getLongitude());
+                            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(currentLatLng,
+                                    15);
+                            map.moveCamera(update);
+                        }
+                    }
+                });
+
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
